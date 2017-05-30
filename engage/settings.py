6f6f9f353fbf8c6fe7e0490d7f248204f58ff71c
Quayce Thomas <quayce@timsle.co>
2017-05-30 07:29:53 -0400

    alter settings

--- engage/settings.py
+++ engage/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'engage.wsgi.application'
 
 #Database
 #https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'engage',
-        'USER': 'postgres',
-        'PASSWORD': 'timsleadmin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'engage',
+#        'USER': 'postgres',
+#        'PASSWORD': 'timsleadmin',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
@@ -128,3 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'tools'
+
+
+DATABASES =  {'default' : dj_database_url.config(
+               default='postgres://snplkgpbswvjht:4c383d0e7ec794e6257534bf0e20584b31446bddb26e4ad0137ef2a87e1d8443@ec2-174-129-224-33.compute-1.amazonaws.com:5432/d2cu5a4q79lcaq')}
